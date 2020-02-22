f9fc93bebf8d80dece6aed9085e9c77aaf35de2d
AkbarovElvin <elvinna@code.edu.az>
2020-02-22 16:32:29 +0400

    lab-task-added

---
+++ js/main.js
@@ -0,0 +1,83 @@
+// var number1=prompt();
+// var number2=prompt();
+
+function numberSum(a,b){
+    
+   return a+b;
+   //console.log(a+b);
+}
+//numberSum(parseInt(number1),parseInt(number2));
+
+
+
+
+
+// numberSum(20,54);
+
+function numberMinus(a,b){
+
+    console.log(a-b);
+}
+
+function numberDiv(a,b){
+    console.log(a/b);
+}
+
+//numberDiv(9,3);
+// numberMinus(9,2);
+
+
+
+console.log(numberSum(5,6));
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+
+
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+
+// numberMinus(9,2);
+// numberMinus(9,2);
+// numberMinus(9,2);
+
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+// numberSum(5,6);
+
+
